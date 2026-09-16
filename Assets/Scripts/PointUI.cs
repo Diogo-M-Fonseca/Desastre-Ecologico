0e056211d165c51de7bb15c0012ac65a60eeeb1a
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2026-09-16 12:39:15 +0100

    Fix tmpro. we´re dumb asf

--- Assets/Scripts/PointUI.cs
+++ Assets/Scripts/PointUI.cs
@@ -1,16 +1,15 @@
-using TMPro;
+
 using UnityEngine;
 
 public class PointUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI textMesh;
 
     private void Update()
     {
         PlayerLogic playerLogic = FindAnyObjectByType<PlayerLogic>();
         if (playerLogic != null)
         {
-            textMesh.text = "Points: " + playerLogic.Points;
+        
         }
     }
 }
