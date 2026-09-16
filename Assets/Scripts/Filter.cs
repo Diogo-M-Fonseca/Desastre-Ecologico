10d88efcae8bcc44f361ce385bdf033f663eb430
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-09-16 12:49:48 +0100

    TMPro problems

--- Assets/Scripts/Filter.cs
+++ Assets/Scripts/Filter.cs
@@ -1,13 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class Filter : MonoBehaviour
 {
     [SerializeField] private PlayerLogic playerLogic;
+    [SerializeField] private TextMeshProUGUI textMeshPro;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerLogic>() == false)
         {
             playerLogic.PointUp();
+            textMeshPro.text = playerLogic.Points.ToString();
             other.gameObject.SetActive(false);
         }
     }
