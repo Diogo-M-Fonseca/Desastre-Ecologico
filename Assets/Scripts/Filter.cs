6864caf6fec6df05af97854d9c0f28c8e130fc8e
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-09-17 10:23:18 +0100

    text fix

--- Assets/Scripts/Filter.cs
+++ Assets/Scripts/Filter.cs
@@ -1,16 +1,13 @@
-using TMPro;
 using UnityEngine;
 
 public class Filter : MonoBehaviour
 {
     [SerializeField] private PlayerLogic playerLogic;
-    [SerializeField] private TextMeshProUGUI textMeshPro;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerLogic>() == false)
         {
             playerLogic.PointUp();
-            textMeshPro.text = playerLogic.Points.ToString();
             other.gameObject.SetActive(false);
         }
     }
--- Assets/Scripts/PointUI.cs
+++ Assets/Scripts/PointUI.cs
@@ -1,15 +1,16 @@
-
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointUI : MonoBehaviour
 {
+    [SerializeField] private Text pointText;
 
     private void Update()
     {
         PlayerLogic playerLogic = FindAnyObjectByType<PlayerLogic>();
         if (playerLogic != null)
         {
-        
+            pointText.text = "Points: " + playerLogic.Points.ToString();
         }
-    }
+    }   
 }
