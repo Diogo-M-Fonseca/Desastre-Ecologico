da25ec52b0fbb22c800db9e9eded3e8f6cad74fc
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2026-09-25 14:55:18 +0100

    sound

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class Catch : MonoBehaviour
 {
     public event Action Extinguish;
+    [SerializeField] private AudioClip clip;
 
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.TryGetComponent(out PlayerCollider limb))
         {
             Extinguish?.Invoke();
+            AudioSource.PlayClipAtPoint(clip, transform.position);
             gameObject.SetActive(false);
         }
     }
